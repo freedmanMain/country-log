bebc69741052a490ad24c4ec3e12dcd6f893e42c
Fishin Maksim <max.freedman.main@gmail.com>
2022-10-01 21:39:42 +0300

    dev-rest-issue-1 Added rest module. Update .gitignore file.

---
+++ rest/src/test/kotlin/io/github/country/log/rest/RestApplicationTest.kt
@@ -0,0 +1,13 @@
+package io.github.country.log.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApplicationTest {
+
+    @Test
+    fun context() {
+
+    }
+}
